be8aa1d966f2678f23c7a3a7c03b7d7d25507b91
Pedro Martinez <pedrocmartinez568@gmail.com>
2022-05-14 12:24:09 -0500

    First commit

---
+++ main.js
@@ -0,0 +1,9 @@
+document.querySelector(".menu-btn").addEventListener("click", () => {
+    document.querySelector(".nav-menu").classList.toggle("show");
+});
+
+ScrollReveal().reveal('.imagen-principal', {delay: 500});
+ScrollReveal().reveal('.mas-deportes', {delay: 700});
+ScrollReveal().reveal('.banner', {delay: 700});
+ScrollReveal().reveal('.social', {delay: 700});
+ScrollReveal().reveal('.footer-links', {delay: 700});
